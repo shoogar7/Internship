66892f7b4c9954caa2f01c8b72f03d4f3b8b3953
PawełB <pawelb602@gmail.com>
2023-06-05 17:37:02 +0200

    [P2-002] Proposition of changes to gather launches data

--- script.js
+++ script.js
@@ -1,121 +1,139 @@
-/*fetch('https://api.spacexdata.com/v3/launches/')
+fetch('https://api.spacexdata.com/v3/launches/')
         .then(response => response.json())
         .then(text => { 
             console.log(text)
+            displayAllLaunches(text);
             }
         ) 
         .catch((error) => {
             console.error('Error:', error);
-        }); */
+        });
+
+    function displayAllLaunches(launches) {   
+
+        // musze sie obracac tez po colID
+        // for po launches
+        // wstawiam w html biore launcha mission
+        // nastepnie biote descption
+        // po forze wrzucam html do cola
+
+        //for launch
+        let cardBody = `<h3>${launch.mission_name}</h3>
+            <h6>Subtitle</h6>
+            <p>Information</p>
+            <a href="">Link</a>`;   
+
+    }
 
 
-fetch('https://api.spacexdata.com/v3/launches/1')
-        .then(response => response.json())
-        .then(text => { 
-            let information=text.mission_name;
-            let title=document.querySelectorAll(".card-title")[0];
-            title.innerHTML=information;
-            information=text.links.mission_patch;
-            document.querySelectorAll(".card-img-top")[0].src=information;
-            information=text.details;
-            document.querySelectorAll(".card-text")[0].innerHTML=information;
-            information=text.links.wikipedia;
-            document.querySelectorAll(".btn-primary")[0].href=information;
-            }
-        ) 
-        .catch((error) => {
-            console.error('Error:', error);
-        }); 
+// fetch('https://api.spacexdata.com/v3/launches/1')
+//         .then(response => response.json())
+//         .then(text => { 
+//             let information=text.mission_name;
+            // let title=document.querySelectorAll(".card-title")[0];
+            // title.innerHTML=information;
+            // information=text.links.mission_patch;
+            // document.querySelectorAll(".card-img-top")[0].src=information;
+            // information=text.details;
+            // document.querySelectorAll(".card-text")[0].innerHTML=information;
+            // information=text.links.wikipedia;
+            // document.querySelectorAll(".btn-primary")[0].href=information;
+//             }
+//         ) 
+//         .catch((error) => {
+//             console.error('Error:', error);
+//         }); 
 
-fetch('https://api.spacexdata.com/v3/launches/2')
-        .then(response => response.json())
-        .then(text => { 
-            let information=text.mission_name;
-            let title=document.querySelectorAll(".card-title")[1];
-            title.innerHTML=information;
-            information=text.links.mission_patch;
-            document.querySelectorAll(".card-img-top")[1].src=information;
-            information=text.details;
-            document.querySelectorAll(".card-text")[1].innerHTML=information;
-            information=text.links.wikipedia;
-            document.querySelectorAll(".btn-primary")[1].href=information;
-            }
-        ) 
-        .catch((error) => {
-            console.error('Error:', error);
-        }); 
+// fetch('https://api.spacexdata.com/v3/launches/2')
+//         .then(response => response.json())
+//         .then(text => { 
+//             let information=text.mission_name;
+//             let title=document.querySelectorAll(".card-title")[1];
+//             title.innerHTML=information;
+//             information=text.links.mission_patch;
+//             document.querySelectorAll(".card-img-top")[1].src=information;
+//             information=text.details;
+//             document.querySelectorAll(".card-text")[1].innerHTML=information;
+//             information=text.links.wikipedia;
+//             document.querySelectorAll(".btn-primary")[1].href=information;
+//             }
+//         ) 
+//         .catch((error) => {
+//             console.error('Error:', error);
+//         }); 
 
-fetch('https://api.spacexdata.com/v3/launches/3')
-        .then(response => response.json())
-        .then(text => { 
-            let information=text.mission_name;
-            let title=document.querySelectorAll(".card-title")[2];
-            title.innerHTML=information;
-            information=text.links.mission_patch;
-            document.querySelectorAll(".card-img-top")[2].src=information;
-            information=text.details;
-            document.querySelectorAll(".card-text")[2].innerHTML=information;
-            information=text.links.wikipedia;
-            document.querySelectorAll(".btn-primary")[2].href=information;
-            }
-        ) 
-        .catch((error) => {
-            console.error('Error:', error);
-        }); 
+// fetch('https://api.spacexdata.com/v3/launches/3')
+//         .then(response => response.json())
+//         .then(text => { 
+//             let information=text.mission_name;
+//             let title=document.querySelectorAll(".card-title")[2];
+//             title.innerHTML=information;
+//             information=text.links.mission_patch;
+//             document.querySelectorAll(".card-img-top")[2].src=information;
+//             information=text.details;
+//             document.querySelectorAll(".card-text")[2].innerHTML=information;
+//             information=text.links.wikipedia;
+//             document.querySelectorAll(".btn-primary")[2].href=information;
+//             }
+//         ) 
+//         .catch((error) => {
+//             console.error('Error:', error);
+//         }); 
 
-fetch('https://api.spacexdata.com/v3/launches/4')
-        .then(response => response.json())
-        .then(text => { 
-            let information=text.mission_name;
-            let title=document.querySelectorAll(".card-title")[3];
-            title.innerHTML=information;
-            information=text.links.mission_patch;
-            document.querySelectorAll(".card-img-top")[3].src=information;
-            information=text.details;
-            document.querySelectorAll(".card-text")[3].innerHTML=information;
-            information=text.links.wikipedia;
-            document.querySelectorAll(".btn-primary")[3].href=information;
-            }
-        ) 
-        .catch((error) => {
-            console.error('Error:', error);
-        }); 
+// fetch('https://api.spacexdata.com/v3/launches/4')
+//         .then(response => response.json())
+//         .then(text => { 
+//             let information=text.mission_name;
+//             let title=document.querySelectorAll(".card-title")[3];
+//             title.innerHTML=information;
+//             information=text.links.mission_patch;
+//             document.querySelectorAll(".card-img-top")[3].src=information;
+//             information=text.details;
+//             document.querySelectorAll(".card-text")[3].innerHTML=information;
+//             information=text.links.wikipedia;
+//             document.querySelectorAll(".btn-primary")[3].href=information;
+//             }
+//         ) 
+//         .catch((error) => {
+//             console.error('Error:', error);
+//         }); 
 
-fetch('https://api.spacexdata.com/v3/launches/8')
-        .then(response => response.json())
-        .then(text => { 
-            let information=text.mission_name;
-            let title=document.querySelectorAll(".card-title")[4];
-            title.innerHTML=information;
-            information=text.links.mission_patch;
-            document.querySelectorAll(".card-img-top")[4].src=information;
-            information=text.details;
-            document.querySelectorAll(".card-text")[4].innerHTML=information;
-            information=text.links.wikipedia;
-            document.querySelectorAll(".btn-primary")[4].href=information;
-            }
-        ) 
-        .catch((error) => {
-            console.error('Error:', error);
-        }); 
+// fetch('https://api.spacexdata.com/v3/launches/8')
+//         .then(response => response.json())
+//         .then(text => { 
+//             let information=text.mission_name;
+//             let title=document.querySelectorAll(".card-title")[4];
+//             title.innerHTML=information;
+//             information=text.links.mission_patch;
+//             document.querySelectorAll(".card-img-top")[4].src=information;
+//             information=text.details;
+//             document.querySelectorAll(".card-text")[4].innerHTML=information;
+//             information=text.links.wikipedia;
+//             document.querySelectorAll(".btn-primary")[4].href=information;
+//             }
+//         ) 
+//         .catch((error) => {
+//             console.error('Error:', error);
+//         }); 
 
-fetch('https://api.spacexdata.com/v3/launches/9')
-        .then(response => response.json())
-        .then(text => { 
-            let information=text.mission_name;
-            let title=document.querySelectorAll(".card-title")[5];
-            title.innerHTML=information;
-            information=text.links.mission_patch;
-            document.querySelectorAll(".card-img-top")[5].src=information;
-            information=text.details;
-            document.querySelectorAll(".card-text")[5].innerHTML=information;
-            information=text.links.wikipedia;
-            document.querySelectorAll(".btn-primary")[5].href=information;
-            }
-        ) 
-        .catch((error) => {
-            console.error('Error:', error);
-        }); 
+// fetch('https://api.spacexdata.com/v3/launches/9')
+//         .then(response => response.json())
+//         .then(text => { 
+//             let information=text.mission_name;
+//             let title=document.querySelectorAll(".card-title")[5];
+//             title.innerHTML=information;
+//             information=text.links.mission_patch;
+//             document.querySelectorAll(".card-img-top")[5].src=information;
+//             information=text.details;
+//             document.querySelectorAll(".card-text")[5].innerHTML=information;
+//             information=text.links.wikipedia;
+//             document.querySelectorAll(".btn-primary")[5].href=information;
+//             }
+//         ) 
+//         .catch((error) => {
+//             console.error('Error:', error);
+//         }); 
+
 /*  function displayLaunch(data)
   {
     const l = data.launch;
