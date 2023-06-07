b199671df0161dc26023db708df5c903c141981d
Mariusz Briegmann <mariuszbrieg@gmail.com>
2023-06-07 16:42:46 +0200

    [P2-002] Implementation of detail mission if exist

--- script.js
+++ script.js
@@ -24,28 +24,27 @@ function displayAllLaunches(launches)
     </div><br>
     `;
     document.getElementsByClassName("container").innerHTML=rowBody;
+    let details = "There's no information about this mission."
     //console.log(document.getElementsByClassName("container").innerHTML);
     for(let j=0; j<3; j++)
     {
       let launchesArr = launches[i];
+
+      if(launchesArr.details !== null) {
+        details = launchesArr.details
+      }
+
       let colBody = `
       <div class="card">
         <img src="${launchesArr.links.mission_patch}" class="card-img-top" alt="mission_patch">
         <div class="card-body">
           <h5 class="card-title">${launchesArr.mission_name} | ${launchesArr.launch_year} | ID: ${launchesArr.flight_number}</h5>
-          <p class="card-text">${launchesArr.details}</p>
+          <p class="card-text">${ details}</p>
           <a href="launch.flight_number" class="card-link">More about ${launchesArr.mission_name}</a>
         </div>
       </div>
       `;
       document.getElementById(colId).innerHTML=colBody;
-      //console.log(document.getElementsByClassName("row align-items-start").innerHTML);
-      //console.log(document.getElementById(colId).innerHTML);
-      if(launchesArr.details==null)
-      {
-        let pBody = "There's no information about this mission.";
-        document.getElementsByClassName("card-text").innerHTML=pBody;
-      }
     }
   }
 }
